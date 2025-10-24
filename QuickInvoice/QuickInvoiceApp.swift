0c33df0ed26032ee974c91e4fd012f9aab29a501
Edwin Segovia <edwin.segovia1@yahoo.com>
2025-10-23 20:34:18 -0700

    Initial Commit

---
+++ QuickInvoice/QuickInvoiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickInvoiceApp.swift
+//  QuickInvoice
+//
+//  Created by Edwin Segovia on 10/23/25.
+//
+
+import SwiftUI
+
+@main
+struct QuickInvoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
