214d9abb804e4d1cb4f4b9700d588dcc1d340950
Bryan Alarcon <bryanosminalarcon@gmail.com>
2025-11-02 14:29:44 -0800

    Merge partner's branch with resolved conflicts

---
+++ Invoice/InvoiceListViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  InvoiceListViewModel.swift
+//  QuickInvoice
+//
+//  Created by Edwin Segovia on 10/30/25.
+//
+
+import Foundation
+
+final class InvoiceListViewModel: ObservableObject {
+    
+    
+    func addInvoice() {
+        print("AddInvoice Button was Pressed")
+    }
+    
+}
---
+++ ProfileView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileView.swift
+//  QuickInvoice
+//
+//  Created by Edwin Segovia on 10/30/25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        NavigationStack {
+            Text("Enter User's Info here")
+                .navigationTitle("Profile")
+        }
+    }
+}
+
+#Preview {
+    ProfileView()
+}
